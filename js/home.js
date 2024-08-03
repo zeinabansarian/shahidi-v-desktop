d2e4ebb978604316fc1cf82e8d0f9839563af631
faezehdev <faezeh0078@gmail.com>
2024-08-03 15:26:36 +0330

    complete seection two of home pagr

--- js/home.js
+++ js/home.js
@@ -1,6 +1,23 @@
+// LOAD BANNE
+gsap.to('.Banner-Slider img',{
+    x:0,
+    opacity:1,
+    ease:'none'
+})
 // BANNER SLIDER
 let swiperBanner = new Swiper(".swiper-Banner", {
     slidesPerView:1,
     spaceBetween:0,
     effect:'slide'
   }); 
+
+// PRODUCT CATEGORY SLIDER
+
+let swiperProject= new Swiper(".swiper-Category", {
+    loop: true,
+    speed: 1000,
+    centeredSlides:true,
+    slidesPerView:4.7,
+    spaceBetween: 40,
+
+  });  

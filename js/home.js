ccaf2924cdcdf870f4a8b600a9cf621ebc67ad21
faezehdev <faezeh0078@gmail.com>
2024-08-03 17:25:45 +0330

    complete section three of home page

--- js/home.js
+++ js/home.js
@@ -21,3 +21,24 @@ let swiperProject= new Swiper(".swiper-Category", {
     spaceBetween: 40,
 
   });  
+
+  let About = document.querySelector('.About-Clr')
+let ImgSRC =About.getAttribute('data-bg')
+About.style.backgroundImage =`url('${ImgSRC}')`
+
+//   PLAY VIDEO
+let video = document.querySelector('video#main')
+let PlayBtn = document.querySelector('.PlayBTN')
+let popup = document.querySelector('.PopUp')
+let CloseBtn = document.querySelector('.PopUp .Close')
+PlayBtn.addEventListener('click',()=>{
+    popup.classList.add('Open')
+    video.classList.add('show')
+    video.play()
+})
+CloseBtn.addEventListener('click',()=>{
+    popup.classList.remove('Open')
+    video.classList.remove('show')   
+    video.pause()
+})
+

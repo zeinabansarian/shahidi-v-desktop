fdede19727fcf1ce3212c4ff774d0a453d8004f0
Zeinab <zeinabansarian6280@gmail.com>
2024-08-04 10:47:19 +0330

    complete article page

---
+++ js/article.js
@@ -0,0 +1,7 @@
+const imagesSlider = new Swiper('.imagesSlider', {
+    // Optional parameters
+    centeredSlides:true,
+    slidesPerView : 1.4,    
+    speed : 1000,
+    spaceBetween:40,
+  });

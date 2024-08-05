ee54adc603aaf2a0c0026ffe17e7160ca9daaff6
faezehdev <faezeh0078@gmail.com>
2024-08-05 17:06:37 +0330

    complete dynamic

--- js/config.js
+++ js/config.js
@@ -9,17 +9,59 @@ Op.addEventListener('click',()=>{
     gsap.to('.Menu-Container .Menu-Item .Menu-Link',{
         y:0,
         opacity:1,
-        stagger:.1
+        stagger:.1,
+      
     })
     gsap.to(leftIMG,{
         y:0,
         opacity:1,
+        
     })
     gsap.to(RightIMG,{
         y:0,
         opacity:1,
     })
 })
+// Images on hover
+let MenuItems = document.querySelectorAll('.DesktopHeader .Menu-Container .Menu-Item')
+let Banner1 = document.querySelector('.Menu-Container .Left .IMG-C img')
+let Banner2 = document.querySelector('.Menu-Container .Right .IMG-C img')
+
+MenuItems.forEach(item=>{
+    item.addEventListener('mouseenter',(event)=>{
+        for(let i =0 ; i< MenuItems.length ; i++){
+            let img1 = item.getAttribute('data-img-1')
+            let img2 = item.getAttribute('data-img-2')
+            Banner1.classList.add('hide')
+            Banner2.classList.add('hide')
+
+            setTimeout(()=>{
+                Banner1.setAttribute('src',img1)
+                Banner2.setAttribute('src',img2)
+                MenuItems[i].classList.remove('show')    
+                Banner1.classList.remove('hide')
+                Banner2.classList.remove('hide')
+                        
+            },1000)
+          
+        }
+        event.currentTarget.classList.toggle('show')
+        })
+        item.addEventListener('mouseleave',(event)=>{
+            for(let i =0 ; i< MenuItems.length ; i++){
+                MenuItems[i].classList.remove('show')
+                let img1 = item.getAttribute('data-img-1')
+                let img2 = item.getAttribute('data-img-2')
+                Banner1.classList.remove('showIMG')   
+                Banner2.classList.remove('showIMG')  
+            }
+            event.currentTarget.classList.toggle('show')
+     })
+
+})
+
+
+
 Close.addEventListener('click',()=>{
     gsap.to('.Menu-Container .Menu-Item .Menu-Link',{
         y:100,
